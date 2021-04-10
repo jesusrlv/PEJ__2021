92d5776834e7b988320ba406fee74fe997a49e8f
jesusrlv <jesusrlv@MacBook-Pro-de-jesusrlv.local>
2021-04-09 20:41:13 -0500

    perfiles 2

--- conn.php
+++ conn.php
@@ -11,7 +11,7 @@
 
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_SERVER_USERNAME', 'root');
+define('DB_SERVER_USERNAME', 'jesusrlv_pej');
 define('DB_SERVER_PASSWORD', '');
 define('DB_DATABASE', 'PEJ21');
 
