26eff7cfdaa8f3c2e6f73996431d88cf6839b186
Mi nombreRodolfo <jesusrlv.informatica@gmail.com>
2021-04-13 10:46:07 -0500

    local y host bd

--- dashboard/prcd/conn_local.php
+++ dashboard/prcd/conn_local.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername="localhost";
-$database="c2PEJ21"; //solo se quitó para conexión remota
-$username="c2cOnf_psic_0307";
-$password="ThmfMTk_G6";
+$database="PEJ21"; //solo se quitó para conexión remota
+$username="jesusrlv_pej";
+$password="";
 
 $conn= new mysqli ($servername,$username,$password,$database); //solo se quitó para conexión remota
 
