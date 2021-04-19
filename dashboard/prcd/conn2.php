2c7fdc1da64d1839024c0144a553822ee8843952
Mi nombreRodolfo <jesusrlv.informatica@gmail.com>
2021-04-19 14:09:22 -0500

    bd

--- dashboard/prcd/conn2.php
+++ dashboard/prcd/conn2.php
@@ -1,10 +1,10 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_SERVER_USERNAME', 'root');
-define('DB_SERVER_PASSWORD', '');
-define('DB_DATABASE', 'PEJ21');
+// define('DB_SERVER', 'localhost');
+// define('DB_SERVER_USERNAME', 'root');
+// define('DB_SERVER_PASSWORD', '');
+// define('DB_DATABASE', 'PEJ21');
 
-$conn = new mysqli(DB_SERVER, DB_SERVER_USERNAME, DB_SERVER_PASSWORD, DB_DATABASE);
+// $conn = new mysqli(DB_SERVER, DB_SERVER_USERNAME, DB_SERVER_PASSWORD, DB_DATABASE);
 
 
 ?>
