20df7bd7a6b04665b989c6d1f99c379c49237970
Mi nombreRodolfo <jesusrlv.informatica@gmail.com>
2021-04-19 14:05:38 -0500

    me la pela

--- conn.php
+++ conn.php
@@ -1,11 +1,11 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_SERVER_USERNAME', 'root');
-define('DB_SERVER_PASSWORD', '');
-define('DB_DATABASE', 'PEJ21');
 
-$conn = new mysqli(DB_SERVER, DB_SERVER_USERNAME, DB_SERVER_PASSWORD, DB_DATABASE);
+// define('DB_SERVER', 'localhost');
+// define('DB_SERVER_USERNAME', 'jesusrlv_pej');
+// define('DB_SERVER_PASSWORD', '');
+// define('DB_DATABASE', 'PEJ21');
 
+// $conn = new mysqli(DB_SERVER, DB_SERVER_USERNAME, DB_SERVER_PASSWORD, DB_DATABASE);
 
 ?>
 
