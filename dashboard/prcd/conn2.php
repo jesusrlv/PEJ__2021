5b9bbbb617dfdfa6b4d7fe85f0bae87e8d3c3ed0
jesusrlv <jesusrlv@MacBook-Pro-de-jesusrlv.local>
2021-04-24 11:50:41 -0500

    conn

--- dashboard/prcd/conn2.php
+++ dashboard/prcd/conn2.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_SERVER_USERNAME', 'jesusrlv_pej');
-define('DB_SERVER_PASSWORD', '');
-define('DB_DATABASE', 'PEJ21');
+// define('DB_SERVER', 'localhost');
+// define('DB_SERVER_USERNAME', 'jesusrlv_pej');
+// define('DB_SERVER_PASSWORD', '');
+// define('DB_DATABASE', 'PEJ21');
 
 $conn = new mysqli(DB_SERVER, DB_SERVER_USERNAME, DB_SERVER_PASSWORD, DB_DATABASE);
 
@@ -10,10 +10,10 @@ $conn = new mysqli(DB_SERVER, DB_SERVER_USERNAME, DB_SERVER_PASSWORD, DB_DATABAS
 ?>
 
 <?php
-// define('DB_SERVER', 'localhost');
-// define('DB_SERVER_USERNAME', 'c2cOnf_psic_0307');
-// define('DB_SERVER_PASSWORD', 'ThmfMTk_G6');
-// define('DB_DATABASE', 'c2PEJ21');
+define('DB_SERVER', 'localhost');
+define('DB_SERVER_USERNAME', 'c2cOnf_psic_0307');
+define('DB_SERVER_PASSWORD', 'ThmfMTk_G6');
+define('DB_DATABASE', 'c2PEJ21');
 
-// $conn = new mysqli(DB_SERVER, DB_SERVER_USERNAME, DB_SERVER_PASSWORD, DB_DATABASE);
+$conn = new mysqli(DB_SERVER, DB_SERVER_USERNAME, DB_SERVER_PASSWORD, DB_DATABASE);
 ?>
