c6e46137a37a090ad24db5b294c7500ae86ba295
Mi nombreRodolfo <jesusrlv.informatica@gmail.com>
2021-04-27 15:59:24 -0500

    reporte jurado 2

--- administrador/reporte_jurado.php
+++ administrador/reporte_jurado.php
@@ -267,7 +267,7 @@ include('../dashboard/prcd/conn.php');
             $categoria = "SELECT categorias.id,categorias.nombre FROM categorias WHERE id ='$id_cat'";
             $resultado_categoria= $conn->query($categoria);
             $row_categoria=$resultado_categoria->fetch_assoc();
-             echo '<td>'.utf8_encode($row_categoria['nombre']).'</td>';
+             echo '<td>'.$row_categoria['nombre'].'</td>';
 
             
             //  echo '<td>'.$row_usr['categoria'].'</td>';
@@ -291,7 +291,7 @@ include('../dashboard/prcd/conn.php');
              $promedio = ($doc1 + $doc2 + $doc3 + $doc4)/4;
 
              echo '<td><strong>'.$promedio.'</strong></td>';
-             echo '<td>'.($row_usr['observaciones']).'</td>';
+             echo '<td>'.$row_usr['observaciones'].'</td>';
 
              echo '</tr>';
         //    }
