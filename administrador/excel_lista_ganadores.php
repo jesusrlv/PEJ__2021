775d85e09264a095fc67e77b0430e0017733434d
jesusrlv <jesusrlv@MacBook-Pro-de-jesusrlv.local>
2021-04-27 21:53:09 -0500

    utf8 excel ganadores 5

--- administrador/excel_lista_ganadores.php
+++ administrador/excel_lista_ganadores.php
@@ -7,6 +7,12 @@ header("Content-Disposition: attachment; filename=$filename");
 header("Pragma: no-cache");
 header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 header("Content-type: text/html; charset=utf8");
+
+echo "
+    <html xmlns:o=\"urn:schemas-microsoft-com:office:office\" xmlns:x=\"urn:schemas-microsoft-com:office:excel\" xmlns=\"http://www.w3.org/TR/REC-html40\">
+    <html>
+    <head><meta http-equiv=\"Content-type\" content=\"text/html;charset=utf-8\" /></head>";
+
 session_start();
 
 include('../dashboard/prcd/conn.php');
