5db4b47d635fd5f4f121636c48883a0492a9b981
jesusrlv <jesusrlvrojo@gmail.com>
2022-05-09 15:31:33 -0500

    card4

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -203,7 +203,7 @@ include('prcd/conn.php');
         <!-- <div class="row row-cols-1 row-cols-md-2 row-sm-2"> -->
         <div class="row">
           <div class="col-sm-6 mb-6">
-            <div class="card text-dark bg-light mb-6" style="max-width: 36rem; height: 18rem; box-shadow: 4px -1px 0px -1px #005eff;">
+            <div class="card text-dark bg-light mb-6" style="max-width: 36rem; height: auto; box-shadow: 4px -1px 0px -1px #005eff;">
               <div class="card-body">
                 <h5 class="card-title text-primary"><i class="bi bi-credit-card-2-front-fill"></i> DATOS PERSONALES</h5>
                 <p class="card-text">Agregar datos del postulante</p>
